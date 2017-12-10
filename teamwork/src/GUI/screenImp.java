bd80792ba887910b233d2c662923b028dbd0c588
linkiloi3 <33095584+linkiloi3@users.noreply.github.com>
2017-12-10 19:56:51 +0700

    Create screenImp.java
    screen implement

---
+++ teamwork/src/GUI/screenImp.java
@@ -0,0 +1,9 @@
+package GUI;
+
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+
+public interface screenImp{
+	public void mousePressed(MouseEvent e);
+	public void render(Graphics g);
+}
