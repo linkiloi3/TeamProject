c52a88ac7e43229317616cee05c2ceb3cb12e182
linkiloi3 <33095584+linkiloi3@users.noreply.github.com>
2017-12-10 20:01:45 +0700

    Update Defeat.java

--- teamwork/src/GUI/Defeat.java
+++ teamwork/src/GUI/Defeat.java
@@ -1,6 +1,7 @@
 package GUI;
 import GameObject.*;
 import Script.Handler;
+import Zombie.Enemy;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -9,52 +10,46 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 
-public class Defeat extends MouseAdapter{
-	
-	
+public class Defeat extends MouseAdapter implements screenImp{
+	private Utility utility = Utility.getInstance();
+	public static void check(){
+		for (int i=0;i<Handler.object.size();i++){
+			GameObject temp;
+			temp=Handler.object.get(i);
+			if (temp instanceof Enemy){
+				if (temp.getX()<150){
+					Handler.removeObject(temp);
+					State.setGameState(ID.Defeat);
+				}
+			}
+		}
+	}
+	@Override
 	public void mousePressed(MouseEvent e){
 		int mx=e.getX();
 		int my=e.getY();
-		if (mouseOver(mx,my,480,300,200,64) && State.getGameState()==ID.Defeat){
+		if(utility.mouseOver(mx,my,480,300,200,64) && State.getGameState()==ID.Defeat){
 			while (Handler.object.size()>0){
 				Handler.object.remove(0);
 			}
 			Player.setSunCredit(50);
 			State.setGameState(ID.Game);
-			Anou.setAnou("Can you survive for 2 minutes");
+			Victory.setStartTime(System.currentTimeMillis());
+			Anou.setAnou("Can you survive for 4 minutes");
 		}
 	}
-	
-	public void mouseRelease(MouseEvent e){
-		
-	}
-	
-	public void tick(){
-
-	}
-	
-	private boolean mouseOver(int mx,int my,int x, int y, int width , int height){
-		if (mx>x && mx < x + width){
-			if (my > y && my< y + height){
-				return true;
-			}
-			else return false;
-		}
-		else return false;
-	}
-	
 	public void render(Graphics g){
-		
+		utility.draw(ObjType.RetryButton, 480-15, 300, g);
 		Font fnt = new Font("arial",1,50);
 		g.setFont(fnt);
 		g.setColor(Color.white);
 		g.drawString("Defeat", 500, 50);
-		
-		g.setFont(fnt);
-		g.setColor(Color.white);
-		g.drawString("Retry", 525, 350);
-		
-		g.setColor(Color.white);
-		g.drawRect(480, 300, 200, 64);
+//		
+//		g.setFont(fnt);
+//		g.setColor(Color.white);
+//		g.drawString("Retry", 525, 350);
+//		
+//		g.setColor(Color.white);
+//		g.drawRect(480, 300, 200, 64);
 	}
 }
