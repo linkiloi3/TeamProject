b8f1a35f97314f7d8c8666c1447ebd5bcc7058bb
knightvvp <33200663+knightvvp@users.noreply.github.com>
2017-12-10 20:47:56 +0700

    Update BucketHead.java

--- teamwork/src/Zombie/BucketHead.java
+++ teamwork/src/Zombie/BucketHead.java
@@ -1,19 +1,20 @@
 package Zombie;
 import Script.Game;
 import Script.SpriteSheet;
-import Script.Utility;
 
 import java.awt.Graphics;
 
 import GUI.ObjType;
+import GameObject.Utility;
 
 public class BucketHead extends Enemy implements ZombieInterface{
 	private static int power=40;
+	private Utility utility=Utility.getInstance();
 	public BucketHead(int x,int y){
 		super(x,y,35,1,15);
 	}
 	public void render(Graphics g){
-		Utility.draw(ObjType.Buckethead, x, y, g);
+		utility.draw(ObjType.Buckethead, x, y, g);
 	}
 	@Override
 	public int getPower() {
