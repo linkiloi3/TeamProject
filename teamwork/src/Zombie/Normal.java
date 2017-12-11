4ea879a8f8e687e66b2beb8827f37e5eed1de3fa
knightvvp <33200663+knightvvp@users.noreply.github.com>
2017-12-11 16:13:26 +0700

    Update Normal.java

--- teamwork/src/Zombie/Normal.java
+++ teamwork/src/Zombie/Normal.java
@@ -3,14 +3,23 @@ package Zombie;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Normal extends Enemy{
-	
+import GUI.ObjType;
+import GameObject.Utility;
+import Script.Game;
+import Script.SpriteSheet;
+
+public class Normal extends Enemy implements ZombieInterface{
+	private static int power=10;
+	private Utility utility=Utility.getInstance();
 	public Normal(int x,int y){
 		super(x,y,7,1,10);
 	}
 	public void render(Graphics g){
-		g.setColor(Color.gray);
-		g.fillRect(x, y, 30, 80);
+		utility.draw(ObjType.Normal, x, y, g);
 		
+	}
+	@Override
+	public int getPower() {
+		return power;
 	}
 }
